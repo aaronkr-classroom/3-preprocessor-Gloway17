93189a4fab7b78ab128c2eb130142424c72f43ce
Gloway17 <ictdhn@gmail.com>
2025-09-30 14:12:13 +0900

    add reset()

--- Week 4/hw.c
+++ Week 4/hw.c
@@ -1,44 +1,55 @@
 // hw.c
 #include <stdio.h>
 
-int main() {
-	int data = 5, result = 0;
+int data = 5, result = 0;
 
+void reset() { data = 5, result = 0;}
+
+int main() {
 	result = data % 2;
 	printf("Q1 result = data %% 2;\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = data < 2;
 	printf("Q2 result = data < 2;\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = data < 4 + 3;
 	printf("Q3 result = data < 4 + 3;\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = data++ + 2;
 	printf("Q4 result = data++ + 2;\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = data == 5;
 	printf("Q5 result = data == 5;\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = data != 5 && (data = 0);
 	printf("Q6 result = data != 5 && (data = 0);\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = --result && (data = 0);
 	printf("Q7 result = --result && (data = 0);\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = result-- && (data = 0);
 	printf("Q8 result = result-- && (data = 0);\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	result = result-- || (data = 0);
 	printf("Q9 result = result-- || (data = 0);\n");
 	printf("\t(result: %d)\n\n", result);
+	reset();
 
 	return 0;
 }
